8c470531c9832923879d82051ee7475d65a6f82c
Matias Rios <matias.hurling@gmail.com>
2023-09-24 02:15:20 -0300

    Commit inicial del DOM de React incluido el primer componente creado

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Header /> 
+      </header>
+      {/* El resto de tu contenido va aquí */}
+    </div>
+  );
+}
+
+export default App;
+
