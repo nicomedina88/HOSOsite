b83aa76256ffd0454c5830cc6c0cc88b87a1b9c8
nicolas <nicolas00medina@hotmail.com>
2024-05-15 11:24:36 -0300

    Completar fusión con ramaNico

--- src/componentes/Carrusel/Carrusel.jsx
+++ src/componentes/Carrusel/Carrusel.jsx
@@ -10,16 +10,6 @@ function Carrusel() {
 
   const data = [
     {
-<<<<<<< HEAD
-=======
-      title: 'Nuestro programa',
-      text: 'El Hospedaje Solidario se encuentra en el marco del programa “La UNPAZ en el mundo, el mundo en la UNPAZ” y tiene como objetivo el intercambio intercultural,  promoviendo así la internacionalización en nuestra comunidad universitaria.',
-      image: foto1,
-      buttonLabel: 'Más información',
-      link: '/sobre-hoso',
-    },
-    {
->>>>>>> fc0b0578fcee30a605f03dcf2b5f881ef84fc25b
       title: 'Sobre HOSO',
       text: 'El Proyecto surge de la necesidad de responder ante la demanda de Hospedaje Solidario por parte de estudiantes, docentes, investigadores y graduados/as interesandos en realizar un período de intercambio en la UNPAZ...',
       image: foto2,
@@ -32,7 +22,6 @@ function Carrusel() {
       image: foto3,
       buttonLabel: 'Más información',
       link: '/sumate',
-<<<<<<< HEAD
     },
     {
       title: 'Nuestro programa',
@@ -40,8 +29,6 @@ function Carrusel() {
       image: foto1,
       buttonLabel: 'Más información',
       link: '/sobre-hoso',
-=======
->>>>>>> fc0b0578fcee30a605f03dcf2b5f881ef84fc25b
     },
   ];
 
