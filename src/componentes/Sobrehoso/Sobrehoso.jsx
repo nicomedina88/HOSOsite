7893ae0fe7ef088c35aa6a6c06602059094efbf1
igorezequiel22 <eze131315@gmail.com>
2024-05-27 18:01:37 -0300

    cambie la descripcion SOBRE HOSO

--- src/componentes/Sobrehoso/Sobrehoso.jsx
+++ src/componentes/Sobrehoso/Sobrehoso.jsx
@@ -19,44 +19,35 @@ const Sobrehoso = () => {
         </div>
       <div className="text-container">
         <p className="paragraph">
-        El hospedaje solidario (HS) se encuentra en el marco del programa “La UNPAZ en el mundo. 
-        El mundo en la UNPAZ” de la Dirección General de Relaciones Internacionales, y tiene como objetivo 
-        el enriquecimiento multicultural, ya que implica la convivencia con estudiantes de distintas partes 
-        de Latinoamérica, a quienes se les ofrece alojamiento ad honorem durante el período de estancia de la 
-        movilidad académica en la UNPAZ, promoviendo así la internacionalización en nuestra comunidad universitaria.
+        El proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de responder ante la demanda
+        de Hospedaje Solidario (HS) por parte de estudiantes, docentes, investigadores/as, graduados/as y/o gestores
+        extranjeros interesados en realizar un período de intercambio en la UNPAZ.
         <br />
         <br />
-        En este marco, el proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de 
-        responder ante la demanda de hospedaje solidario por parte de estudiantes, docentes, investigadores/as, 
-        graduados/as y/o gestores extranjeros interesados en realizar un período de intercambio en la UNPAZ.
+       “HOSO”, como la plataforma web multipantalla, viene a proponer un espacio donde almacenar, consultar y modificar
+        los datos de los/as ANFITRIONES/AS de una forma eficiente, facilitando la búsqueda y el contacto de los mismos a fin de 
+        conocer su disponibilidad para recibir a un/a RESIDENTE en su hogar durante el período de intercambio.
         <br />
         <br />
-        “HOSO”, como la plataforma web multipantalla, viene a proponer un espacio donde almacenar, 
-        consultar y modificar los datos de los/as ANFITRIONES/AS de una forma eficiente, facilitando la 
-        búsqueda y el contacto de los mismos a fin de conocer su disponibilidad para recibir a un/a RESIDENTE 
-        en su hogar durante el período de intercambio.
+        
         </p>
       </div>
       </div>
       <div className="text-with-image-container-mob">
       <div className="text-container">
       <p className="paragraph">
-        El hospedaje solidario (HS) se encuentra en el marco del programa “La UNPAZ en el mundo. 
-        El mundo en la UNPAZ” de la Dirección General de Relaciones Internacionales, y tiene como objetivo 
-        el enriquecimiento multicultural, ya que implica la convivencia con estudiantes de distintas partes 
-        de Latinoamérica, a quienes se les ofrece alojamiento ad honorem durante el período de estancia de la 
-        movilidad académica en la UNPAZ, promoviendo así la internacionalización en nuestra comunidad universitaria.
-        <br />
-        <br />
-        En este marco, el proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de 
-        responder ante la demanda de hospedaje solidario por parte de estudiantes, docentes, investigadores/as, 
-        graduados/as y/o gestores extranjeros interesados en realizar un período de intercambio en la UNPAZ.
+        El proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de responder
+         ante la demanda de Hospedaje Solidario (HS) por parte de estudiantes, docentes, investigadores/as,
+          graduados/as y/o gestores extranjeros interesados en realizar
+           un período de intercambio en la UNPAZ.
         <br />
         <br />
         “HOSO”, como la plataforma web multipantalla, viene a proponer un espacio donde almacenar, 
         consultar y modificar los datos de los/as ANFITRIONES/AS de una forma eficiente, facilitando la 
         búsqueda y el contacto de los mismos a fin de conocer su disponibilidad para recibir a un/a RESIDENTE 
         en su hogar durante el período de intercambio.
+        <br />
+        <br />
         </p>
       </div>
       <div className="logo-container1">
