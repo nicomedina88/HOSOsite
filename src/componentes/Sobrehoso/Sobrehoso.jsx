0d7f9d1f56ea7c67edff897b1c1d454ac8976001
nicolas <nicolas00medina@hotmail.com>
2024-05-28 23:09:35 -0300

    cambios2

--- src/componentes/Sobrehoso/Sobrehoso.jsx
+++ src/componentes/Sobrehoso/Sobrehoso.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Sobrehoso.css';
 import logo6 from '../../img/hoso7.jpg';
 import img2 from '../../img/RGDI3.jpg';
+import internacionales from '../../img/HOSOPresentaciónoficial.jpg';
 
 const Sobrehoso = () => {
   return (
@@ -19,44 +20,35 @@ const Sobrehoso = () => {
         </div>
       <div className="text-container">
         <p className="paragraph">
-        El hospedaje solidario (HS) se encuentra en el marco del programa “La UNPAZ en el mundo. 
-        El mundo en la UNPAZ” de la Dirección General de Relaciones Internacionales, y tiene como objetivo 
-        el enriquecimiento multicultural, ya que implica la convivencia con estudiantes de distintas partes 
-        de Latinoamérica, a quienes se les ofrece alojamiento ad honorem durante el período de estancia de la 
-        movilidad académica en la UNPAZ, promoviendo así la internacionalización en nuestra comunidad universitaria.
+        El proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de responder ante la demanda
+        de Hospedaje Solidario (HS) por parte de estudiantes, docentes, investigadores/as, graduados/as y/o gestores
+        extranjeros interesados en realizar un período de intercambio en la UNPAZ.
         <br />
         <br />
-        En este marco, el proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de 
-        responder ante la demanda de hospedaje solidario por parte de estudiantes, docentes, investigadores/as, 
-        graduados/as y/o gestores extranjeros interesados en realizar un período de intercambio en la UNPAZ.
+       “HOSO”, como la plataforma web multipantalla, viene a proponer un espacio donde almacenar, consultar y modificar
+        los datos de los/as ANFITRIONES/AS de una forma eficiente, facilitando la búsqueda y el contacto de los mismos a fin de 
+        conocer su disponibilidad para recibir a un/a RESIDENTE en su hogar durante el período de intercambio.
         <br />
         <br />
-        “HOSO”, como la plataforma web multipantalla, viene a proponer un espacio donde almacenar, 
-        consultar y modificar los datos de los/as ANFITRIONES/AS de una forma eficiente, facilitando la 
-        búsqueda y el contacto de los mismos a fin de conocer su disponibilidad para recibir a un/a RESIDENTE 
-        en su hogar durante el período de intercambio.
+        
         </p>
       </div>
       </div>
       <div className="text-with-image-container-mob">
       <div className="text-container">
       <p className="paragraph">
-        El hospedaje solidario (HS) se encuentra en el marco del programa “La UNPAZ en el mundo. 
-        El mundo en la UNPAZ” de la Dirección General de Relaciones Internacionales, y tiene como objetivo 
-        el enriquecimiento multicultural, ya que implica la convivencia con estudiantes de distintas partes 
-        de Latinoamérica, a quienes se les ofrece alojamiento ad honorem durante el período de estancia de la 
-        movilidad académica en la UNPAZ, promoviendo así la internacionalización en nuestra comunidad universitaria.
-        <br />
-        <br />
-        En este marco, el proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de 
-        responder ante la demanda de hospedaje solidario por parte de estudiantes, docentes, investigadores/as, 
-        graduados/as y/o gestores extranjeros interesados en realizar un período de intercambio en la UNPAZ.
+        El proyecto “HOSO. Abrazando a estudiantes del mundo” surge de la necesidad de responder
+         ante la demanda de Hospedaje Solidario (HS) por parte de estudiantes, docentes, investigadores/as,
+          graduados/as y/o gestores extranjeros interesados en realizar
+           un período de intercambio en la UNPAZ.
         <br />
         <br />
         “HOSO”, como la plataforma web multipantalla, viene a proponer un espacio donde almacenar, 
         consultar y modificar los datos de los/as ANFITRIONES/AS de una forma eficiente, facilitando la 
         búsqueda y el contacto de los mismos a fin de conocer su disponibilidad para recibir a un/a RESIDENTE 
         en su hogar durante el período de intercambio.
+        <br />
+        <br />
         </p>
       </div>
       <div className="logo-container1">
@@ -95,7 +87,41 @@ const Sobrehoso = () => {
           alt="Logo del proyecto HOSO"
           className="ediuni"
         />
+      </div>     
+      <div className="sobrehoso-container"></div>
+      <div className="mini-banner3">
+        <div className="title-container3">
+          <h1 className="title">Nuestro Programa</h1>
+        </div>
       </div>
+      <br />
+      <div className="text-container">
+        <p className="paragraph"> 
+        El hospedaje solidario (HS) se encuentra en el marco del programa “La UNPAZ en el mundo. El mundo en la UNPAZ” de la Dirección 
+        General de Relaciones Internacionales, y tiene como objetivo el enriquecimiento multicultural, ya que implica la convivencia con estudiantes de distintas partes de Latinoamérica, a quienes se les ofrece alojamiento ad honorem durante el período de estancia de la movilidad académica en la UNPAZ, promoviendo así la internacionalización en nuestra comunidad universitaria.
+    </p>
+    <img src={internacionales} alt="logo-unpaz" className="unpaz"/>
+    <br />
+    <p className="paragraph">
+     Este Programa fue aprobado por resolución N° 38/2022- que enmarca al REGLAMENTO DE ALOJAMIENTO SOLIDARIO -aprobado por Resolución CS N°154/2019 y su modificatoria R. CS N°53/2023 que prevé el pago de un “aporte para gastos de servicios básicos”-, institucionalizando el ofrecimiento de un hospedaje solidario (HS) para estudiantes, docentes, investigadores/as y gestores/as que realicen una estancia académica en la Universidad Nacional de José C. Paz.
+    <br />
+   <br />
+     Este reglamento determina como actores participantes del HS a los ANFITRIONES (personas que ofrecen el alojamiento), y los RESIDENTES (personas movilizadas que realizan una estancia académica en la UNPAZ). En tal sentido, considerando el avance de las nuevas tecnologías como medio de acceso y difusión que potencian el alcance y transferencia con el territorio, se planificó el desarrollo de una plataforma web que permita conectar eficientemente a los RESIDENTES con los potenciales ANFITRIONES brindando información valiosa y actualizada, y contando con la posibilidad de extender los límites de la convocatoria vigente.
+   </p>
+    </div>
+    <div className="title-container3"></div>
+    <h1 className="title">¿Que es el alojamiento solidario?</h1> 
+    <div className="text-container">
+    <p className="paragraph"> 
+    Es una experiencia para recibir como ANFITRIONES a estudiantes de movilidad internacional en su casa durante un semestre académico. Tiene como objetivo el enriquecimiento multicultural ya que implica la convivencia con estudiantes de otros países a quienes se les ofrece un hogar durante el período de estancia del intercambio académico en la UNPAZ, promoviendo la internacionalización en nuestra comunidad universitaria.
+    </p>
+    <p className="paragraph"> 
+    Conoce nuestras <a href="https://drive.google.com/file/d/1EGXuhbCYEtrxTw33qIIA1GUFY7hL8u-s/view">Practicas  de convivencia.</a> 
+    <br />
+    <br />
+    Si querés obtener más información, visitá la <a href="https://www.unpaz.edu.ar/internacionalizacion-integral">página del programa.</a>  
+    </p>
+    </div>
     </div>
   );
 };
